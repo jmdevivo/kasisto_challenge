b454475f6862dfc8e85dc101c174365cc8d0acc4
jmdevivo <jdevivo@gmail.com>
2015-07-30 11:44:47 -0400

    First two types of speech bubble implemented as Handlbar templates

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello, World");
+var userMsgTemplate = Handlebars.templates['userMessageTmpl'];
+
+var context = { "message": "Pay Rashav $10,000"};
+
+$('#content').html(userMsgTemplate(context));
+
+var whichPersonTemplate = Handlebars.templates['whichPersonTmpl'];
+
+var whichContext = {
+	'name': "John",
+	'people': [
+		{'url': "images/emptyContact.jpg", 'contactName':"John Devivo", "phoneNumber": "516 761 0464"},
+		{'url': "", 'contactName':"John Michael Devivo", "phoneNumber": "516 761 0464"},
+	],
+}
+
+$('#content').append(whichPersonTemplate(whichContext));
